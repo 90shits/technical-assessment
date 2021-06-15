c06ce1032cde65d6022ae5b701de51061c2a829e
Quyen Luu <luqu0501@gmail.com>
2021-06-15 22:22:28 +0700

    feat: stage 1, show survey list

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    surveyList: []
   },
 
   getters: {
+    getSurveyList (state) {
+      return state.surveyList
+    }
   },
 
   mutations: {
+    setSurveyList (state, surveyList) {
+      // Sort by survey code, a greater than b then 1 & otherwise
+      state.surveyList = surveyList.sort((a, b) => (a.code > b.code) ? 1 : -1)
+    }
   },
 
   actions: {
+    async fetchSurveyList ({ commit }) {
+      const { data } = await Vue.axios.get('/api/list.json')
+      commit('setSurveyList', data)
+    }
   }
 })
